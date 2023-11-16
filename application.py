d000d84fdf5c6b56a84a8751728940bf64967937
jelelz <jelelfaith14@gmail.com>
2023-11-16 11:20:03 +0300

    Added physician files and modified patient's login

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 import json
 from pymongo import MongoClient
 import hashlib
 import urllib.parse
 
 # Creating a new Flask app for MediSafe
-app = Flask(__name__)
+app: Flask = Flask(__name__)
 
 # Escape the username and password
 username = urllib.parse.quote_plus("faithkimokiy")
@@ -20,10 +20,10 @@ collection = db['patient_record']
 
 # a new blockchain function to store the data in blockchain format
 def blockchain(data):
-    # Generate a hash of the data.
+    # Generating a hash of the data.
     hash1 = hashlib.sha256(json.dumps(data).encode('utf-8')).hexdigest()
 
-    # Store the hash of the data in MongoDB.
+    # Storing the hash of the data in MongoDB.
     collection.insert_one({
         'hash': hash1
     })
@@ -51,11 +51,6 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/patient_auth', methods=['GET'])
-def patient_auth():
-    return render_template('patient_auth.html')
-
-
 @app.route('/register', methods=['POST'])
 def register():
     name = request.args.get('name')
@@ -73,37 +68,70 @@ def register():
     return jsonify({'success': True})
 
 
-@app.route('/login', methods=['POST'])
-def login():
-    email = request.form['email']
-    password = request.form['password']
+@app.route('/patient_auth', methods=['GET', 'POST'])
+def patient_auth():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
 
-    # Find the user record in the database.
-    user = collection.find_one({'email': email})
+        # Find the user record in the database.
+        user = collection.find_one({'email': email})
 
-    # Check if the user exists and the password is correct.
-    if user and user['password'] == password:
-        # Retrieve the record hash from the database
-        record_hash = user['hash']
+        # Check if the user exists and the password is correct.
+        if user and user['password'] == password:
+            # Retrieve the EHR record from MongoDB
+            ehr_record = collection.find_one({'hash': user['hash']})
 
-        # Generate the hash from the provided email and password
-        provided_hash = hashlib.sha256(email.encode('utf-8') + password.encode('utf-8')).hexdigest()
-
-        # Compare the hashes to validate the email and password
-        if record_hash == provided_hash:
-            # Login the user and display success message
-            return render_template('patient_dash.html')
+            # Return the EHR record to the user
+            return jsonify({'ehr_record': ehr_record})
         else:
-            # Hash mismatch, display error message
-            return render_template('error.html', message="Invalid email or password")
+            # User not found or password incorrect, display error message
+            return jsonify({'error': 'Invalid email or password'})
     else:
-        # User not found or password incorrect, display error message
-        return render_template('error.html', message="Invalid email or password")
+        return render_template('patient_auth.html')
 
 
-@app.route('/physician')
-def physician():
-    return render_template('physician.html')
+# Define a list of registered patients
+registered_patients = [
+    {"id": 1, "name": "John Doe"},
+    {"id": 2, "name": "Jane Smith"},
+    {"id": 3, "name": "Peter Jones"},
+]
+
+# Define a dictionary to store patient medical records
+patient_records = {
+    1: {"medical_history": "Lorem ipsum dolor sit amet"},
+    2: {"medical_history": "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"},
+    3: {
+        "medical_history": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"},
+}
+
+
+# Route for the physician app
+@app.route('/physician', methods=['GET', 'POST'])
+def physician_app():
+    if request.method == 'POST':
+        patient_id = int(request.form['patient_id'])
+
+        # Check if the patient is registered
+        if patient_id in patient_records:
+            # Grant access to the patient's medical record
+            return redirect(url_for('physician_view', patient_id=patient_id))
+        else:
+            error_message = "Patient not found"
+            return render_template('physician.html', registered_patients=registered_patients,
+                                   error_message=error_message)
+
+    return render_template('physician.html', registered_patients=registered_patients)
+
+
+# Route for viewing a patient's medical record
+@app.route('/physician_view/<int:patient_id>')
+def view_records(patient_id):
+    # Retrieve the patient's medical record
+    medical_history = patient_records[patient_id]['medical_history']
+
+    return render_template('physician_view.html', patient_id=patient_id, medical_history=medical_history)
 
 
 @app.route('/records')
